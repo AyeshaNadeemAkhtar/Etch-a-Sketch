dca1f0630065c07595b6703cec70d2e6c28df10a
AyeshaNadeemAkhtar <ayshanadeemakhtar@gmail.com>
2025-05-06 18:41:07 +0500

    First Prototype for Etch-A-Sketch
    Still  need to add Progressive Opacity + restructure Reduncdant Code

--- script.js
+++ script.js
@@ -1,95 +1,86 @@
-function createSquares()
+const container = document.querySelector("#container");
+let div;
+
+
+// Generate 16-by-16 grid
+for (let i = 1; i <= 16; i++)
 {
-    const container = document.querySelector("#container");
+  for (let j = 1; j <= 16; j++)
+  {
+    div = document.createElement("div");
     
-    let currentDiv;
-
-    /* Default Divs */
-    for (let i = 0; i < 16; i++)
-    {
-        for (let j = 0; j < 16; j++)
-        {
-            currentDiv = document.createElement("div");
-            currentDiv.setAttribute("id", `div${j + 1}`);
-            currentDiv.setAttribute("class", "boxes");
-            container.append(currentDiv);
-    
-        }
-        
-
-    }
-
-    /* Mouse Enter for default divs*/
-    const clickableDivs = document.querySelectorAll(".boxes");
-    clickableDivs.forEach((div) => {
-        div.addEventListener("mouseenter", function () {
-            div.style.backgroundColor = "black";
-            div.style.color = "white";
-        });
-    });
-
-   
-    /* Button to ask for div */
-    const button = document.querySelector("#ask");
-    button.addEventListener("click", function () {
-        const numberSquares = prompt("No. of squares for the new Grid: (Not more than 100)");
-
-        /* If no. is not greater than 100 */
-        if (numberSquares <= 100) {
-            /* Remove the old one */
-            clickableDivs.forEach((div) => {
-                div.remove();
-            });
-    
-            /*Calculate the width for each box */
-            let widthNewElement = 100 / numberSquares;
-           
-           
-            /* BUilt the boxes */
-            for (let j = 0; j < numberSquares; j++)
-            {
-                for (let k = 0; k < numberSquares; k++)
-                {
-                    currentDiv = document.createElement("div");
-                    currentDiv.style.width = widthNewElement + "%";
-                    currentDiv.setAttribute("id", `div${k + 1}`);
-                    currentDiv.setAttribute("class", "newBoxes");
-                    container.append(currentDiv);
-                }
-                
-            }
-           
-            
-
-            /* Select new Divs for hover effect*/
-            const newClickableDivs = document.querySelectorAll(".newBoxes");
-            /* let currentOpacity = Number(window.getComputedStyle(div).opacity);*/
-          
-            newClickableDivs.forEach((div) => {
-                div.count = 0;
-                
-                div.addEventListener("mouseenter", function () {
-                    div.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
-                    div.count++;
-                    if (div.count === 10)
-                    {
-                        div.style.backgroundColor = "black";
-                    }
-                });
-            });
-
-            // If we remove 1 from total Opacity it erases so we get an eraser..
-
-        }
-        else
-        {
-            alert("Squares shouldn't be more than 10");
-        }
-       
-    });
+    div.setAttribute("class", `div${j}`);
+    div.setAttribute("class", "initialDivs")
+    container.appendChild(div);
+  }
 }
 
-createSquares();
+
+// Activate trailing effect
+const allDivs = container.querySelectorAll("div");
+allDivs.forEach(function(div) {
+  div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = "black";
+  });
+});
 
 
+// Clickable button
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let alertSquares = parseInt(prompt("No. of squares: "));
+  if (alertSquares > 100)
+  {
+    alert("You can choose b/w 1 and 100");
+  }
 
+  // Remove initial divs
+  allDivs.forEach(function(div) {
+    div.remove();
+  })
+
+
+  
+  // Generate new Divs
+  for (let i = 1; i <= alertSquares; i++)
+    {
+      for (let j = 1; j <= alertSquares; j++)
+      {
+        div = document.createElement("div");
+        
+        div.setAttribute("class", `div${j}`);
+        div.setAttribute("class", `recentDivs`);
+        container.appendChild(div);
+      }
+    }
+
+    button.disabled = true;
+    
+
+    // Do flex on recentDivs
+    let flexBasis = 100 / alertSquares + '%';
+    const finalAllDivs = container.querySelectorAll(".recentDivs");
+
+
+    for(let div of finalAllDivs)
+    {
+      div.style.flex = `1 0 ${flexBasis}`;
+
+      div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = getRandomColors();
+      });
+    };
+
+    
+});
+
+
+// Get randomColors
+function getRandomColors() {
+  console.log("Random Function is called"); /* bp */
+  let r = Math.floor(Math.random() * 256) ;
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
